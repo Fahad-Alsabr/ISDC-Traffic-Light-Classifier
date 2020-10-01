fd0763f9791cd0f56e664cb3b056fb44ed131275
Fahad-Alsabr <fhd0_0>
2020-10-01 22:27:08 +0300

    Traffic-Light-Classifier

---
+++ helpers.py
@@ -0,0 +1,53 @@
+# Helper functions
+
+import os
+import glob # library for loading images from a directory
+import matplotlib.image as mpimg
+
+
+
+
+
+# This function loads in images and their labels and places them in a list
+# The list contains all images and their associated labels
+# For example, after data is loaded, im_list[0][:] will be the first image-label pair in the list
+def load_dataset(image_dir):
+    
+    # Populate this empty image list
+    im_list = []
+    image_types = ["red", "yellow", "green"]
+    
+    # Iterate through each color folder
+    for im_type in image_types:
+        
+        # Iterate through each image file in each image_type folder
+        # glob reads in any image with the extension "image_dir/im_type/*"
+        for file in glob.glob(os.path.join(image_dir, im_type, "*")):
+            
+            # Read in the image
+            im = mpimg.imread(file)
+            
+            # Check if the image exists/if it's been correctly read-in
+            if not im is None:
+                # Append the image, and it's type (red, green, yellow) to the image list
+                im_list.append((im, im_type))
+
+    return im_list
+
+def encode(label):
+    one_hot = []
+    numerical_val = [0,0,0]
+    if (label == 'red' ):
+        numerical_val = [1,0,1]
+    if(label == 'yellow'):
+        numerical_val = [0,1,0]
+    if (label == 'green'):
+        
+        numerical_val = [0,1,0]
+    
+    
+    one_hot = numerical_val
+    
+    return one_hot
+
+
